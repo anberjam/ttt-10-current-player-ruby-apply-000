dbf7d798584429ba65f937f5487247c87cad969c
Anber Jamshed <anberjam@yahoo.com>
2020-08-03 01:47:15 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,7 +8,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board)%2==0 || turn_count(board)==0
+  if (turn_count(board)%2==0) || (turn_count(board)==0)
     return "X"
   else
     return "O"
